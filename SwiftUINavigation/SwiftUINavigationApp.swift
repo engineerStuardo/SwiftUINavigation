50eaa05c7388fe95060963e9faa611d32ef19526
engineerStuardo <ingeniero.stuardo@gmail.com>
2023-06-08 05:42:11 -0600

    Initial Commit

---
+++ SwiftUINavigation/SwiftUINavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigationApp.swift
+//  SwiftUINavigation
+//
+//  Created by Italo Stuardo on 8/6/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
